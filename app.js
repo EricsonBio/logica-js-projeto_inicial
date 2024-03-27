7364ef9b4acf976c1e5444df4f20f249bd063b90
EricsonBio <Ericson.bio@gmail.com>
2024-03-27 11:22:13 -0300

    UpdateJSgame

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
-// alert inserir um texto
-alert('Bem vindo ao jogo, tente desccobrir o numero secreto');
-// let utilizado para declarar uma variavel, utiliza-se o sinal de "=" para atribuir o valor
-let numeroSecreto = Math.floor(Math.random() * 15);
-let numeroTentativas = 0;
-// if usa para testar uma logica
-while (numeroTentativas <= 2) {
-    let numeroEscolhido = parseInt(prompt('Digite um numero de 0 a 15'));
-    if (numeroSecreto == numeroEscolhido) {
-        alert(`PARABÉNS VOCÊ ACERTOU O NUMERO ${numeroSecreto}`);
+//inserir texto de incio
+// add numeros de tentativas, numero secreto.
+//alert('BEM VINDO AO JOGO');
+//alert('TENTE ACERTAR O NUMERO SECRETO');
+
+let numeroSecreto = Math.floor(Math.random() * 9);
+let tentativas = 3;
+
+while (tentativas > 0) {
+    let numeroDigitado = parseInt(prompt('DIGITE UM NUMERO DE 0 A 9'));
+    if (numeroDigitado === numeroSecreto) {
+        alert(`PARABENS VOCÊ ACERTOU O ${numeroSecreto} AGORA VAMOS SUBIR O NÍVEL`);
+        numeroSecreto = Math.floor(Math.random() * 20);
+        tentativas = 5;
+        while (tentativas > 0) {
+            let numeroDigitado = parseInt(prompt('DIGITE UM NUMERO DE 0 A 20'));
+            if (numeroDigitado === numeroSecreto) {
+                alert(`PARABENS VOCÊ ACERTOU O ${numeroSecreto}`);
+                break;
+            } else if (numeroDigitado > numeroSecreto) {
+                alert(`VOCÊ DIGITOU ${numeroDigitado} QUE É MAIOR QUE O NUMERO SECRETO`);
+            } else {
+                alert(`VOCÊ DIGITOU ${numeroDigitado} QUE É MENOR QUE O NUMERO SECRETO`);
+            }
+            tentativas = tentativas - 1;
+            alert(`VOCÊ AINDA TEM ${tentativas}`);
+        }
         break;
-    } else if (numeroEscolhido > numeroSecreto) {
-        alert(`Você digitou ${numeroEscolhido} que é maior que o numero secreto`);
+    } else if (numeroDigitado > numeroSecreto) {
+        alert(`VOCÊ DIGITOU ${numeroDigitado} QUE É MAIOR QUE O NUMERO SECRETO`);
     } else {
-        alert(`Você digitou ${numeroEscolhido} que é menor que o numero secreto`);
+        alert(`VOCÊ DIGITOU ${numeroDigitado} QUE É MENOR QUE O NUMERO SECRETO`);
     }
-    numeroTentativas = numeroTentativas + 1;
-}       
+    tentativas = tentativas - 1;
+    alert(`VOCÊ AINDA TEM ${tentativas}`);
+}
+
+
