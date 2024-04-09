5dcad8611935141462776dbecc6e65e598f90e94
EricsonBio <Ericson.bio@gmail.com>
2024-04-09 11:28:35 -0300

    att

--- 1-maior18.js
+++ 1-maior18.js
@@ -1,7 +1,8 @@
+//Seleciona a idade do individuo
 let maioridade = 18;
 let nome = prompt('NOME');
 let idade= parseInt(prompt('IDADE'))
-
+//Responde se é maior ou menor de idade
 if (idade >= maioridade) {
     alert ('VOCE É MAIOR DE IDADE')
 } else {
